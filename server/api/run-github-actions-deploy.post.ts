5b34d34c0a13c5a199d392f580e61582757ab1ab
timothe joubert <timothejoubert26@gmail.com>
2025-03-31 18:37:00 +0200

    fix: fix request method

--- server/api/run-github-actions-deploy.post.ts
+++ server/api/run-github-actions-deploy.post.ts
@@ -2,13 +2,13 @@ export default defineEventHandler((event) => {
     const { githubUserToken } = useRuntimeConfig(event)
 
     return $fetch('https://api.github.com/repos/timothejoubert/julie-guzal/dispatches', {
-        method: 'post',
+        method: 'POST',
         headers: {
             Accept: 'application/vnd.github+json',
             Authorization: `token ${githubUserToken}`,
         },
         body: {
-            event_type: 'on_access_run_github_actions_deploy_route',
+            event_type: 'run_github_actions_deploy_from_custom_endpoint',
             client_payload: {
                 unit: false,
                 integration: true,
